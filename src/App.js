72010587b960fe2709e2a1952da75f8b0d5fc338
Nasim Akhtar <81704562+nasim601@users.noreply.github.com>
2024-01-25 00:14:14 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Main from './components/Main';
+import './components/style.css';
+import Card from './components/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
